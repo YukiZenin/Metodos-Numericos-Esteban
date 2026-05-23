8eddebf70856ad4a6f8a73bd5d8193bf64eae52d
YukiZenin <109053214+YukiZenin@users.noreply.github.com>
2026-05-22 19:28:50 -0600

    Delete Unidad_1/3-Codigos/Error_redondeo_binario.java

--- Unidad_1/3-Codigos/Error_redondeo_binario.java
+++
@@ -1,8 +0,0 @@
-package error_redondeo_binario;
-public class Error_redondeo_binario {
-    public static void main(String[] args) {
-        System.out.println(0.2+0.3);
-        System.out.println("Erp");
-    }
-    
-}
